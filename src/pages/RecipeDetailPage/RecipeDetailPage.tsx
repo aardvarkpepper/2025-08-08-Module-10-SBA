a2bc09d7c4e6c8ae8774d7a648a6dc314ef38b26
James Lee <aardvarkpepper@gmail.com>
2025-08-10 04:08:10 -0400

    More styling, again (for Recipe Detail Page)

--- src/pages/RecipeDetailPage/RecipeDetailPage.tsx
+++ src/pages/RecipeDetailPage/RecipeDetailPage.tsx
@@ -36,20 +36,27 @@ export const RecipeDetailPage = () => {
   }
 
   return (
-    <div>
-      <h2>{mealRef.strMeal}</h2>
-      {isListedInFavorites(mealRef.idMeal) ? <button onClick={() => removeRecipe(mealRef.idMeal)}>Remove from Favorites</button>: <button onClick={()=>addRecipe(mealRef.idMeal, mealRef.strMeal)}>Add to Favorites</button>}
-      <div>Category: {mealRef.strCategory}</div>
-      <div>Area: {mealRef.strArea}</div>
-      <div>Instructions: {mealRef.strInstructions}</div>
-      {mealRef.strMealThumb ? <img src={mealRef.strMealThumb} alt={mealRef.strMeal} /> : null}
-      <div>Tags: {mealRef.strTags}</div>
-      {mealRef.strYoutube ? <iframe width="420" height="315"
-        src={`https://www.youtube.com/embed/${mealRef.strYoutube.slice(32)}`}>
-      </iframe> : null}
-      <ul>
-        {ingredientArray.map(ingredient => ingredient[0] ? <li key={`${mealRef.idMeal}-${ingredient[0]}`}>{ingredient[0]}: {ingredient[1]}</li> : null)}
-      </ul>
+    <div className='border-container'>
+      <div className='flexh jc-spacebetween'>
+        <h2>{mealRef.strMeal}</h2>
+        {isListedInFavorites(mealRef.idMeal) ? <button onClick={() => removeRecipe(mealRef.idMeal)}>Remove from Favorites</button> : <button onClick={() => addRecipe(mealRef.idMeal, mealRef.strMeal)}>Add to Favorites</button>}
+      </div>
+      <div className='fontheavy'>Category: {mealRef.strCategory}</div>
+      <div className='fontheavy'>Area: {mealRef.strArea}</div>
+      {mealRef.strMealThumb ? <img className='img-float' src={mealRef.strMealThumb} alt={mealRef.strMeal} /> : null}
+      {mealRef.strTags ? <div>Tags: {mealRef.strTags}</div> : null}
+      <div><span>Instructions:</span><span>&nbsp;&nbsp;&nbsp;{mealRef.strInstructions}</span></div>
+      <div className='clear'></div>
+      <div>
+        <div className='img-float-right video-container'>
+          {mealRef.strYoutube ? <iframe width="420" height="315"
+            src={`https://www.youtube.com/embed/${mealRef.strYoutube.slice(32)}`}>
+          </iframe> : null}
+        </div>
+        <ul className='border-container padding-plus'>
+          {ingredientArray.map(ingredient => ingredient[0] ? <li key={`${mealRef.idMeal}-${ingredient[0]}`}>{ingredient[0]}: {ingredient[1]}</li> : null)}
+        </ul>
+      </div>
     </div>
   )
 }
